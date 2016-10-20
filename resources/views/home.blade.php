c53c7b6f0c064dea1c15fee657163cf65b598113
Alex Barnett <barnetas92@gmail.com>
2016-10-20 22:58:37 +1000

    Added a mock stats to user page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,10 +8,28 @@
                 <div class="panel-heading">Dashboard</div>
 
                 <div class="panel-body">
-                    You are logged in!
-                </div>
+                    <div class="wrap">
+                        <h4>Your Stats</h4>
+                        <div class="holder">
+                            <div class="bar cf" data-ratio=".2">
+                                <span class="label">SOCIAL</span></div>
+                            <div class="bar cf" data-ratio=".5">
+                                <span class="label">POWER</span></div>
+                            <div class="bar cf" data-ratio="-.3"><span class="label">ECONOMIC</span></div>
+                            <div class="bar cf" data-ratio="-.5"><span class="label">WAR</span></div>
+                            <div class="bar cf" data-ratio="-.2"><span class="label">ENVIRONMENT</span></div>
+                            <div class="verticalLine" data-ratio="-.5">VL</div>
+                            <div class="verticalLine" data-ratio="-.3">L</div>
+                            <div class="verticalLine" data-ratio="-.1">C</div>
+                            <div class="verticalLine" data-ratio=".1">R</div>
+                            <div class="verticalLine" data-ratio=".3">VR</div>
+
+
+
+                        </div>
             </div>
         </div>
     </div>
 </div>
+    <script src="js/bar.js"></script>
 @endsection
